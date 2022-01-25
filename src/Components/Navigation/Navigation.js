7d985fca61b72f59fdcfd3ca6c49932806a46738
AliReza <AliRezaDavodiofficial@yahoo.com>
2022-01-25 17:07:07 +0330

    added beat animation to the cart

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 import Cart from "../../assests/cart.png";
 import Like from "../../assests/heart.png";
-import Card from "../Card/Card";
 
 import css from "./Navigation.module.scss";
 
 const Navigation = () => {
   const [toggle, settoggle] = useState(false);
+  const [cartBeat, setCartBeat] = useState(false);
+
+  const cartFoods = useSelector((state) => state.cart.foods);
+
+  useEffect(() => {
+    if (cartFoods.length === 0) return;
+    setCartBeat(true);
+    const timer = setTimeout(() => {
+      setCartBeat(false);
+    }, 350);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartFoods]);
 
   return (
     <section>
@@ -49,7 +63,11 @@ const Navigation = () => {
         <div className={css.personal}>
           <div className={css["personal-icon"]}>
             <NavLink activeClassName={css.active} to="/cart">
-              <img src={Cart} alt="cart icon" />
+              <img
+                src={Cart}
+                alt="cart icon"
+                className={`${cartBeat ? "beatUp" : ""}`}
+              />
             </NavLink>
           </div>
           <div className={css["personal-icon"]}>
