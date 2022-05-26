723a9943f1df051ae361190d921527a10d2c58dd
AliReza <AliRezaDavodiofficial@gmail.com>
2022-05-26 21:31:27 +0430

    added apiConfig

---
+++ src/router/api-config.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+
+// apiKeys
+export const spoonacularApiKey = "apiKey=8cc208ac129d4067aca765018108052d";
+export const firebaseApiKey = "?key=AIzaSyASJnoIJ35f3ZyTjKHaY9xZnP9TnKVhjaE";
+
+// Urls
+export const spoonacularGetFood = "https://api.spoonacular.com/recipes/random?";
+export const firebaseSignup =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp";
+export const firebaseSignin =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword";
+export const firebaseRealDataBase =
+  "https://orderit-75ad5-default-rtdb.firebaseio.com/";
+
+
+const api = {
+    food : {
+        foods : _ => axios.get(`${spoonacularGetFood}number=100&${spoonacularApiKey}`).then(res => res.data),
+        info : data => axios.get(`https://api.spoonacular.com/recipes/${data.foodID}/information?includeNutrition=false${spoonacularApiKey}`).then(res => res.data)
+    },
+    
+    auth : {
+        singup : data => axios.post(`${firebaseSignup}${firebaseApiKey}`,data).then(res => res.data),
+        singin : data => axios.post(`${firebaseSignin}${firebaseApiKey}`,data).then(res => res.data)
+    },
+
+
+    comment : {
+        getComments : data => axios.get(`${firebaseRealDataBase}/comment/${data.foodID}.json` ).then(res => res.data),
+        addComment : data => axios.post(`${firebaseRealDataBase}/comment/${data.foodID}.json`, data).then(res => res.data)
+    }
+}
+
+
+
+export default api
