f0f8bc4e3cacde3618399c401bd9c58ed9b022ed
AliReza <AliRezaDavodiofficial@gmail.com>
2022-07-27 13:18:16 +0430

    moved apiConfig to it's folder

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,18 +1,17 @@
 import axios from "axios"
 
 // apiKeys
-export const spoonacularApiKey = "&apiKey=8cc208ac129d4067aca765018108052d";
-export const firebaseApiKey = "?key=AIzaSyASJnoIJ35f3ZyTjKHaY9xZnP9TnKVhjaE";
+export const spoonacularApiKey = "&apiKey=8cc208ac129d4067aca765018108052d"
+export const firebaseApiKey = "?key=AIzaSyASJnoIJ35f3ZyTjKHaY9xZnP9TnKVhjaE"
 
 // Urls
-export const spoonacularGetFood = "https://api.spoonacular.com/recipes/random?";
+export const spoonacularGetFood = "https://api.spoonacular.com/recipes/random?"
 export const firebaseSignup =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp";
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
 export const firebaseSignin =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword";
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
 export const firebaseRealDataBase =
-  "https://orderit-75ad5-default-rtdb.firebaseio.com/";
-
+  "https://orderit-75ad5-default-rtdb.firebaseio.com/"
 
 const api = {
   food: {
@@ -64,7 +63,5 @@ const api = {
         }),
   },
 }
-
-
 
 export default api
---
+++ src/router/allRouter.js
@@ -0,0 +1,42 @@
+import Auth from "../pages/auth"
+import Cart from "../pages/cart"
+import Favorite from "../pages/favorite"
+import Foods from "../pages/foods"
+import FoodsDetails from "../pages/foods-details"
+import Home from "../pages/home"
+
+export const userRoute = [
+  {
+    path: "/cart",
+    component: Cart,
+    exact: true,
+  },
+  {
+    path: "/favorite",
+    component: Favorite,
+    exact: true,
+  },
+  {
+    path: "/foods",
+    component: Foods,
+    exact: true,
+  },
+  {
+    path: "/foods/:foodID",
+    component: FoodsDetails,
+    exact: false,
+  },
+]
+
+export const authRoute = [
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+  },
+  {
+    path: "/auth",
+    component: Auth,
+    exact: true,
+  },
+]
